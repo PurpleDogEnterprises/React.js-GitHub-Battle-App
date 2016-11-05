289d8a2ab30a9973294160ba1ad0f4f7c4f0051f
Sean Guthrie (Purple Dog Enterprises) <seanpatrickguthrie@gmail.com>
2016-11-05 13:21:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin')
-var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: __dirname + '/app/index.html',
-  filename: 'index.html',
-  inject: 'body'
+var USER_DATA = {
+	name: "Sean Guthrie",
+	username: "PurpleDogEnterprises",
+	image: "https://avatars3.githubusercontent.com/u/19189749?v=3&s=466"
+}
+
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+var ProfilePic = React.createClass({
+	render: function () {
+		return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+	}
 });
 
-module.exports = {
-  entry: [
-    './app/index.js'
-  ],
-  output: {
-    path: __dirname + '/dist',
-    filename: "index_bundle.js"
-  },
-  module: {
-    loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
-    ]
-  },
-  plugins: [HTMLWebpackPluginConfig]
-};
+var ProfileLink = React.createClass({
+	render: function () {
+		return (
+			<div>
+				<a href={"https://www.github.com/" + this.props.username}>
+					{this.props.username}
+				</a>
+			</div>
+		)
+	}
+});
+
+var ProfileName = React.createClass({
+	render: function () {
+		return (
+			<div>{this.props.name}</div>
+		)
+	}
+});
+
+var Avatar = React.createClass({
+	render: function () {
+		return (
+			<div>
+				<ProfilePic imageUrl={this.props.user.image} />
+				<ProfileName name={this.props.user.name} />
+				<ProfileLink username={this.props.user.username} />
+			</div>
+		)
+	}
+})
+
+ReactDOM.render(
+	<Avatar user
